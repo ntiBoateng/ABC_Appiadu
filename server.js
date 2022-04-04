ae6ec1a47cdf38e59540aeb2a06375eed9e3f677
ntiBoateng <gideon.boateng@amalitech.org>
2022-04-04 08:24:32 -0800

    make it better

---
+++ server.js
@@ -0,0 +1,144 @@
+require("dotenv").config();
+require('./models/db');
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Handlebars = require('handlebars')
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const hbsS = require("hbs")
+var hbs = require('express-handlebars')
+const path = require("path")
+const bodyparser = require('body-parser');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const app = express();
+
+const employeeController = require('./controllers/employeeController');
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyparser.json());
+
+
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
+} = require("./middlewares/auth");
+
+
+
+const initializePassport = require("./passport-config");
+initializePassport(
+  passport,
+  async (email) => {
+    const userFound = await User.findOne({ email });
+    return userFound;
+  },
+  async (id) => {
+    const userFound = await User.findOne({ _id: id });
+    return userFound;
+  }
+);
+
+//hbs template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+  extname: 'hbs',
+  defaultLayout: false,
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
+  layoutsDir: __dirname + '/views/layouts/',
+  // partialsDir: __dirname + '/views/partials'
+}))
+
+
+//ejs template engine
+//app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+
+app.use('/employee',checkAuthenticated, employeeController);
+
+app.get("/", (req, res) => {
+  res.render("index");  
+});
+
+
+app.get("/contact",(req,res)=>{
+  res.render("contact")
+})
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login");
+});
+
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+  const userFound = await User.findOne({ email: req.body.email });
+
+  if (userFound) {
+    req.flash("error", "User with that email already exists");
+    res.redirect("/register");
+  } else {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+
+      await user.save();
+      res.redirect("/login");
+    } catch (error) {
+      console.log(error);
+      res.redirect("/register");
+    }
+  }
+});
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+mongoose
+  .connect("mongodb+srv://bgnti:Aladdin200@cluster0.vrp2h.mongodb.net/EmployeeDB", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on Port 3000");
+    });
+  });
